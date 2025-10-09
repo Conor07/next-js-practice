26ca15a70b7b822f5de580d29b38534b9a18cb7e
Conor Talbot <conor@Conors-MacBook-Pro-2.local>
2025-10-09 09:49:09 +0100

    Removed unnecessary practice files

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import Image from "next/image";
 import Link from "next/link";
-import ProductCard from "./components/ProductCard";
 
 export default function Home() {
   return (
@@ -8,8 +7,6 @@ export default function Home() {
       <h1>Hello</h1>
 
       <Link href="/users">Users</Link>
-
-      <ProductCard />
     </main>
   );
 }
